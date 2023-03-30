14d4e32e4d7794b5db743087146c390ddd176528
francisco-leiva <leivafrancisco631@gmail.com>
2023-03-30 14:55:21 -0300

    style: adding background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: '#8c8fde',
+      },
+    },
   },
   plugins: [],
 };
