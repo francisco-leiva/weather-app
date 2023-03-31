546ab880dbe71e9a6c7558a7950eca208f5f0d48
francisco-leiva <leivafrancisco631@gmail.com>
2023-03-31 12:36:38 -0300

    Change where the api is called

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 
 function App() {
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+  const apiURL = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Rosario&days=7&aqi=no&alerts=no`;
+  const [currentWeather, setCurrentWeather] = useState([]);
+  const [location, setLocation] = useState([]);
+  const [forecast, setForecast] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(apiURL);
+
+      setCurrentWeather(response.data.current);
+      setLocation(response.data.location);
+      setForecast(response.data.forecast.forecastday);
+    };
+
+    getData();
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header
+        currentWeather={currentWeather}
+        location={location}
+        forecast={forecast[0]}
+      />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,37 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => {
-  const [currentWeather, setCurrentWeather] = useState([]);
-  const [location, setLocation] = useState([]);
-  const [forecast, setForecast] = useState([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      const response = await axios.get(
-        'http://api.weatherapi.com/v1/forecast.json?key=294a7374210646ddac2205214232903&q=Rosario&days=7'
-      );
-
-      setCurrentWeather(response.data.current);
-      setLocation(response.data.location);
-      setForecast(response.data.forecast.forecastday);
-    };
-
-    getData();
-  }, []);
-
+const Header = ({ currentWeather, location, forecast }) => {
   return (
     <header className='flex items-center justify-around bg-violet'>
-      <div>
-        <h2 className='text-5xl'>{currentWeather && currentWeather.temp_c}º</h2>
-        <span className='text-2xl'>{location && location.name}</span>
+      <div className='text-white'>
+        <h2 className='text-5xl'>{currentWeather?.temp_c}º</h2>
+        <span className='text-2xl'>{location?.name}</span>
 
         <p>
-          {forecast &&
-            `${Math.round(forecast[0].day.maxtemp_c)}º / ${Math.round(
-              forecast[0].day.mintemp_c
-            )}`}
-          {currentWeather &&
-            ` Feels like ${Math.round(currentWeather.feelslike_c)}º`}
+          {`${Math.round(forecast?.day?.maxtemp_c)}º / ${Math.round(
+            forecast?.day?.mintemp_c
+          )}`}
+
+          {` Feels like ${Math.round(currentWeather?.feelslike_c)}º`}
         </p>
       </div>
 
       <picture>
         <img
-          src={currentWeather.condition.icon}
-          alt={currentWeather.condition.text}
+          src={currentWeather?.condition?.icon}
+          alt={currentWeather?.condition?.text}
           width='86rem'
           height='86rem'
         />
       </picture>
     </header>
   );
+};
+
+Header.propTypes = {
+  currentWeather: PropTypes.object,
+  location: PropTypes.object,
+  forecast: PropTypes.object,
 };
 
 export default Header;
