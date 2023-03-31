4739e4208bee902803ebe00e9bfb4533c2744db3
francisco-leiva <leivafrancisco631@gmail.com>
2023-03-31 19:09:23 -0300

    Fix api call problem

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HourlyForecast from './components/HourlyForcast';
 
 function App() {
   const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
-  const apiURL = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Rosario&days=7&aqi=no&alerts=no`;
+  const apiURL = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Rosario&days=7&aqi=no&alerts=no`;
   const [currentWeather, setCurrentWeather] = useState([]);
   const [location, setLocation] = useState([]);
   const [forecast, setForecast] = useState([]);
