4a4a7788fcb1341535557a1c137a3080c92abc29
francisco-leiva <leivafrancisco631@gmail.com>
2023-04-01 00:20:25 -0300

    Add MUI library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import HourlyForecast from './components/HourlyForcast';
 
 function App() {
-  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
-  const apiURL = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Rosario&days=7&aqi=no&alerts=no`;
+  const apiURL = `https://api.weatherapi.com/v1/forecast.json?key=27108248a8404184a5222207233103&q=Rosario&days=7&aqi=no&alerts=no`;
   const [currentWeather, setCurrentWeather] = useState([]);
   const [location, setLocation] = useState([]);
   const [forecast, setForecast] = useState([]);
@@ -22,7 +23,14 @@ function App() {
     getData();
   }, []);
 
-  return (
+  return currentWeather.length === 0 ? (
+    <Backdrop
+      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={true}
+    >
+      <CircularProgress color='inherit' />
+    </Backdrop>
+  ) : (
     <div className='bg-violet'>
       <Header
         currentWeather={currentWeather}
