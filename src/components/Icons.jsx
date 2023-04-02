9c1af9040d046d34764da0aa1a505617aa917c32
francisco-leiva <leivafrancisco631@gmail.com>
2023-04-01 21:00:46 -0300

    Add new icons

---
+++ src/components/Icons.jsx
@@ -0,0 +1,55 @@
+export const DropOfWater = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='icon icon-tabler icon-tabler-droplet-filled'
+      width={16}
+      height={16}
+      viewBox='0 0 24 24'
+      strokeWidth='2'
+      stroke='currentColor'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+      <path
+        d='M6.801 11.003a6 6 0 1 0 10.396 -.003l-5.197 -8l-5.199 8.003z'
+        stroke='#010202'
+        strokeWidth={0}
+        fill='currentColor'
+      ></path>
+      <path d='M12 3v17' strokeWidth={0} fill='currentColor'></path>
+      <path d='M12 12l3.544 -3.544' strokeWidth={0} fill='currentColor'></path>
+      <path
+        d='M12 17.3l5.558 -5.558'
+        strokeWidth={0}
+        fill='currentColor'
+      ></path>
+    </svg>
+  );
+};
+
+export const MapPin = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='icon icon-tabler icon-tabler-map-pin-filled'
+      width={24}
+      height={24}
+      viewBox='0 0 24 24'
+      strokeWidth='2'
+      stroke='currentColor'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+      <path
+        d='M18.364 4.636a9 9 0 0 1 .203 12.519l-.203 .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9 9 0 0 1 12.728 -12.728zm-6.364 3.364a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z'
+        strokeWidth={0}
+        fill='currentColor'
+      ></path>
+    </svg>
+  );
+};
