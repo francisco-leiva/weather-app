3a265e8aeada14d42a963775a8ed934d2486a837
francisco-leiva <leivafrancisco631@gmail.com>
2024-04-24 11:37:52 -0300

    refactor: upgrade of data fetching for different cases depending on the coords

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -4,7 +4,7 @@ import { fetchWeather } from '../services/api';
 export function useWeather() {
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(true);
-  const [coords, setCoords] = useState(null);
+  const [coords, setCoords] = useState('');
   const prevCoords = localStorage.getItem('coords');
 
   useEffect(() => {
@@ -13,12 +13,13 @@ export function useWeather() {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          const coordsString = latitude + ',' + longitude;
+          // regex to get 6 numbers after the comma
+          const regExp = /^-?\d+(?:\.\d{0,6})?/;
+          const shortLatitude = latitude.toString().match(regExp)[0];
+          const shortLongitude = longitude.toString().match(regExp)[0];
+          const coordsString = shortLatitude + ',' + shortLongitude;
 
-          // setting coords
           setCoords(coordsString);
-          // save location in localStorage
-          localStorage.setItem('coords', coordsString);
         },
         (e) => {
           console.error('Could not get your location', e);
@@ -28,7 +29,20 @@ export function useWeather() {
   }, []);
 
   useEffect(() => {
-    if (!coords && prevCoords) {
+    if (coords && coords === prevCoords) return;
+
+    if (coords) {
+      setLoading(true);
+      fetchWeather(coords).then((data) => {
+        setWeather(data);
+        setLoading(false);
+      });
+
+      localStorage.setItem('coords', coords);
+      return;
+    }
+
+    if (prevCoords) {
       setLoading(true);
       fetchWeather(prevCoords).then((data) => {
         setWeather(data);
@@ -37,23 +51,14 @@ export function useWeather() {
       return;
     }
 
-    if (coords && coords !== prevCoords) {
-      setLoading(true);
-      fetchWeather(coords).then((data) => {
-        setWeather(data);
-        setLoading(false);
-      });
-      return;
-    }
-
     // default location
-    if (!coords && !prevCoords) {
-      setLoading(true);
-      fetchWeather('Rosario').then((data) => {
-        setWeather(data);
-        setLoading(false);
-      });
-    }
+    setLoading(true);
+    fetchWeather('-32.945805, -60.653698').then((data) => {
+      setWeather(data);
+      setLoading(false);
+    });
+
+    localStorage.setItem('coords', '-32.945805, -60.653698');
   }, [coords]);
 
   return { weather, loading };
